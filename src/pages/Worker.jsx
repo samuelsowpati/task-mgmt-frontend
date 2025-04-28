d3fbd37eacc250c3ff195184e271512b5b696f36
samuelsowpati <sowpatisamuel@gmail.com>
2025-04-28 17:56:54 -0400

    final

--- src/pages/Worker.jsx
+++ src/pages/Worker.jsx
@@ -170,7 +170,7 @@ export default function Worker() {
       // Distance in meters
       const meterDistance = distance * 1000;
       // console.log("Distance in meters for :", group.name, ":", meterDistance);
-      toast.info(`Distance in meters for ${group.name}: ${meterDistance.toFixed(2)}`);
+      // toast.info(`Distance in meters for ${group.name}: ${meterDistance.toFixed(2)}`);
       // True if distance is less than 5 meters
       const result = meterDistance <= 2;
       
