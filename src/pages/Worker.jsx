d41f987ff52567a437264f9f5f07c1e040afbc2c
samuelsowpati <sowpatisamuel@gmail.com>
2025-04-28 18:00:19 -0400

    final1

--- src/pages/Worker.jsx
+++ src/pages/Worker.jsx
@@ -172,7 +172,7 @@ export default function Worker() {
       // console.log("Distance in meters for :", group.name, ":", meterDistance);
       // toast.info(`Distance in meters for ${group.name}: ${meterDistance.toFixed(2)}`);
       // True if distance is less than 5 meters
-      const result = meterDistance <= 2;
+      const result = meterDistance <= 5;
       
       // Log the group when location is within range
       if (result === true) {
